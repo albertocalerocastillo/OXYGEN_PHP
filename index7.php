2f4e2d674b2ef02611a4fc0788a5611cd1f34f1b
albertocalerocastillo <albertocalerocastillo@gmail.com>
2025-03-12 17:29:38 +0100

    ADD: Index 7

---
+++ index7.php
@@ -0,0 +1,54 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "root";
+$dbname = "habitaciones";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Conexión fallida: " . $conn->connect_error);
+}
+
+if (isset($_GET['search'])) {
+    $search = $_GET['search'];
+
+    $sql = "SELECT name, number, price, offerPrice FROM habitaciones WHERE name LIKE ?";
+    $stmt = $conn->prepare($sql);
+    $searchParam = "%" . $search . "%";
+    $stmt->bind_param("s", $searchParam);
+    $stmt->execute();
+    $result = $stmt->get_result();
+} else {
+    $sql = "SELECT name, number, price, offerPrice FROM habitaciones";
+    $result = $conn->query($sql);
+}
+
+$habitaciones = [];
+
+if ($result->num_rows > 0) {
+    while ($row = $result->fetch_assoc()) {
+        $habitaciones[] = $row;
+    }
+} else {
+    echo "0 resultados";
+}
+
+$conn->close();
+?>
+
+<form>
+    <input type="text" name="search" placeholder="Buscar habitación">
+    <button type="submit">Buscar</button>
+</form>
+
+<ol>
+    <?php foreach ($habitaciones as $habitacion): ?>
+        <li>
+            <strong>Name:</strong> <?php echo $habitacion['name']; ?><br>
+            <strong>Number:</strong> <?php echo $habitacion['number']; ?><br>
+            <strong>Price:</strong> <?php echo $habitacion['price']; ?><br>
+            <strong>Discount:</strong> <?php echo $habitacion['offerPrice']; ?>
+        </li>
+    <?php endforeach; ?>
+</ol>
